568f3b6ebd4756d0963e20bbfebb6bd16db58958
bitflame <monazzam@protonmail.com>
2024-05-04 17:16:40 -0400

    added the code for loops and listst

---
+++ loops.py
@@ -0,0 +1,24 @@
+
+my_favorite_pazzas = ['peperoni', 'white', 'cheese']
+for pizza in my_favorite_pazzas:
+    print("I like",pizza.title(),"Pizza")
+print("Pizza is a neutritious delicious food that is not too difficult to make, and it does not take too long " +
+      "the cheese provides protein and calcium and the tomato sauce has vitamins like vitamin C. The crust provides" +
+      "carbs for energy. I just love pizzas and you can not go wrong with them.")
+
+mamals = ['wolf', 'dog', 'cat']
+for animal in mamals:
+    print("A",animal," is a mamal.")
+    
+# a for loop that counts from 1 to 20 
+for i in range(1, 21):
+    print(i)
+
+myNumbers = list(range(1, 1000001))
+print(min(myNumbers))
+print(max(myNumbers))
+print(sum(myNumbers))
+
+odd_numbers = list(range(1, 21, 2))
+print(odd_numbers)
+    
