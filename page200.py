d3ff2bed687620f32a583164c0f496f3fb8e3ff9
bitflame <monazzam@protonmail.com>
2024-05-15 16:29:49 -0400

    added more code.

--- page200.py
+++ page200.py
@@ -1,9 +1,29 @@
+from pathlib import Path
 # 10-6 write a program that adds two numbers the user inputs
 print('This program will add the nubmers you type in')
+response = 'y'
+while response=='y':
+    try:
+        value_1 = int(input('enter the first value please.'))
+        value_2 = int(input('enter the second value please. '))
+    except ValueError:
+        print('The value you enter has to be a number')
+    else:
+        print(f'The sum of {value_1} and {value_2} is {value_1 + value_2}')
+        response = input('another round? (y/n) ')
+path = Path('cats.txt')
 try:
-    value_1 = int(input('enter the first value please. '))
-    value_2 = int(input('enter the second value please. '))
-except ValueError:
-    print('The value you enter has to be a number')
-else:
-    print(f'The sum of {value_1} and {value_2} is {value_1 + value_2}')
+    content = path.read_text().splitlines()
+except FileNotFoundError:
+    pass
+print('Here is a list of cat names: ')
+for line in content:
+    print(line)
+path = Path('dogs.txt')
+try:
+    content = path.read_text().splitlines()
+except FileNotFoundError:
+    pass
+print('Here is a list of dog names: ')
+for line in content:
+    print(line)
